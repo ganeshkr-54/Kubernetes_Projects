d8c2ea676b036c718a764ef45936e54051f553e0
Ganesh Kumar Reddy <158801596+ganeshkr-54@users.noreply.github.com>
2025-05-13 18:19:54 +0530

    Create 06-UserManagementMicroservice-Deployment-Service.tf

---
+++ Storage/EKS Storage with EBS - Elastic Block Store/06-UserManagementMicroservice-Deployment-Service.tf
@@ -0,0 +1,37 @@
+apiVersion: apps/v1
+kind: Deployment 
+metadata:
+  name: usermgmt-microservice
+  labels:
+    app: usermgmt-restapp
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: usermgmt-restapp
+  template:  
+    metadata:
+      labels: 
+        app: usermgmt-restapp
+    spec:
+      containers:
+        - name: usermgmt-restapp
+          image: stacksimplify/kube-usermanagement-microservice:1.0.0
+          ports: 
+            - containerPort: 8095 
+      initContainers:
+        - name: init-mysql
+          image: busybox:1.28
+          command: ['sh', '-c', "until nslookup mysql; do echo waiting for mysqlservice; sleep 2; done"]
+          
+          env:
+            - name: DB_HOSTNAME
+              value: "mysql"            
+            - name: DB_PORT
+              value: "3306"            
+            - name: DB_NAME
+              value: "usermgmt"            
+            - name: DB_USERNAME
+              value: "root"            
+            - name: DB_PASSWORD
+              value: "dbpassword11"            
